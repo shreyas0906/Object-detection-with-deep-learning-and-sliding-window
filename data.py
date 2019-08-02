41f6120c601a0b1e9a4e593af2a7c658c0ece769
shreyas0906 <shreyas0906@gmail.com>
2019-08-02 01:27:58 -0700

    first refactor

---
+++ data.py
@@ -0,0 +1,93 @@
+#importing the required libraries for image processing
+import cv2
+import numpy as np
+import os
+from argparse import ArgumentParser
+
+test_images = []
+file_images = []
+train_neg_images = []
+
+#Specifying the paths to respective folders
+def main(args):
+
+    train_img_label = []
+    testLabels = []
+    test_img_label = []
+
+# TODO: write a script to rename all the images in a folder to a particular name.
+
+def makeTrainingImages():
+    i = 0
+    for f in os.listdir(args.trainData):
+        if f.endswith('.jpeg') or f.endswith('.jpg') or f.endswith('.png'):
+            imagesFiles.append(f)
+        if f.split('.')[0][0:3] == 'trn': # assigning the label 1 to the cropped images. Note that the file name differentiates between
+            train_img_label.append(1)#the positive and negative images
+        else:
+            train_img_label.append(0)# assigning the label 0 to the negative images.
+    trainImgs = np.ndarray((len(file_images), 1, image_width, image_height))/255 # normalizing the image
+
+    os.chdir(train_pos_path)
+    for file in file_images:
+        if file.endswith('.jpeg') or file.endswith('.jpg') or file.endswith('.png'):
+            img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+            # ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+            trainImgs[i] = np.asarray(img)
+            i+=1
+
+    testLabels = np.asarray(train_img_label)# creating a numpy array for training labels
+
+    for l in os.listdir(testing_path):
+        if l.endswith('.jpeg') or l.endswith('.jpg') or l.endswith('.png'):
+            train_neg_images.append(l)
+        if l.split('.')[0][0:3] == 'trn':
+            test_img_label.append(1)
+        else:
+            test_img_label.append(0)
+    testImgs = np.ndarray((len(train_neg_images), 1, image_width, image_height))/255 # normalizing the image
+
+    i = 0
+    os.chdir(testing_path)
+    for num in train_neg_images:
+        if num.endswith('.jpeg') or num.endswith('.jpg') or num.endswith('.png'):
+            neg_img = cv2.imread(num, cv2.IMREAD_GRAYSCALE)
+            # ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+            testImgs[i] = np.asarray(neg_img)
+            i+=1
+
+    testLabels = np.asarray(test_img_label)
+
+    return testLabels, testImgs, trainLabels, trainImgs
+
+testLabels, testImgs, trainLabels, trainImgs = make_training_images()
+
+#os.chdir(orig_path)
+
+def saveData():
+
+    print("Saving the testing images and labels into a npy file...")
+    np.save("testLabels.npy",testLabels)
+    np.save('testingImg.npy', testImgs)
+    print("Saving the training images and labels into a npy file...")
+    np.save("trainLabels.npy",trainLabels)
+    np.save('trainImgs.npy', trainImgs)
+
+
+if __name__ =='__main__':
+
+        p = ArgumentParser()
+        p.add_argument('trainData', required=True, help='name of training data folder')
+        p.add_argument('testData', required=True, help='name of testing data folder')
+        p.add_argument('objectDim', required=False, help='size of object to be detected. default=45', default=45)
+        args = p.parse_args()
+        
+# Check the number of directories in training data. if more than two folders raise error.
+# testing data. 
+    
+
+
+
+
+
+
